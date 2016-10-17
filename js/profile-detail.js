e763ad25054acf65a125ce590ce20d3c27807557
ericwelshdev <ericwelsh.dev.gmail.com>
2016-10-16 22:45:41 -0700

    v3

---
+++ js/profile-detail.js
@@ -0,0 +1,15 @@
+﻿
+
+$(document).ready(function () {
+
+    $("#image-carousel").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
+
+    });
+
+});
+
